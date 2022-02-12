a7e382c0ff0c83e8fd9cc8959f292fef56aee6f1
Dia <balaska.klaudiaa@gmail.com>
2022-02-13 00:59:17 +0100

    PictureController and requests in progress

--- app/Http/Controllers/Api/PictureController.php
+++ app/Http/Controllers/Api/PictureController.php
@@ -1,10 +1,78 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Picture;
+use App\Http\Requests\PictureRequest;
+use App\Http\Requests\PictureUpdateRequest;
+use Illuminate\Support\Facades\Validator;
 
 class PictureController extends Controller
 {
-    //
+    public function index()
+    {
+        $picture = Picture::all();
+        return response()->json($picture);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), (new PictureRequest())->rules());
+        if  ($validator->fails()) {
+            $errormsg = "";
+            foreach ($validator->errors()->all() as $error) {
+                $errormsg .= $error . " ";
+            }
+            $errormsg = trim($errormsg);
+            return response()->json($errormsg, 400);
+        }
+
+        $picture = new Picture();
+        $picture->fill($request->all());
+        $picture->save();
+        return response()->json($picture, 201);
+    }
+
+    public function show(int $id)
+    {
+        $picture = Picture::find($id);
+        if (is_null($picture)) {
+            return response()->json(["message" => "A megadott azonosítóval nem található kép."], 404);
+        }
+        return response()->json($picture);
+    }
+
+    public function update(PictureUpdateRequest $request, int $id)
+    {
+        if ($request->isMethod('PUT')) {
+            $validator = Validator::make($request->all(), (new PictureUpdateRequest())->rules());
+            if ($validator->fails()) {
+                $errormsg = "";
+                foreach ($validator->errors()->all() as $error) {
+                    $errormsg .= $error . " ";
+                }
+                $errormsg = trim($errormsg);
+                return response()->json($errormsg, 400);
+            }
+        }
+        $picture = Picture::find($id);
+        if (is_null($picture)) {
+            return response()->json(["message" => "A megadott azonosítóval nem található kép."], 404);
+        }
+        $picture->fill($request->all());
+        $picture->save();
+        return response()->json($picture, 200);
+    }
+
+    public function destroy(int $id)
+    {
+        $picture = Picture::find($id);
+        if (is_null($picture)) {
+            return response()->json(["message" => "A megadott azonosítóval nem található kép."], 404);
+        }
+        Picture::destroy($id);
+        return response()->noContent();
+    }
 }
---
+++ app/Http/Requests/PictureRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PictureRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'url' => 'required|string|max:255',
+            'coordinate' => 'required|string',
+            'title' => 'required|string',
+            'description' => 'required',
+            'created_at' => 'required',
+            'updated_at' => 'required',
+        ];
+    }
+}
