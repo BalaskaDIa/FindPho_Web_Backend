edbda814ad5e5ba74166852f3d48e9252bafc7fd
Dia <balaska.klaudiaa@gmail.com>
2022-03-24 19:46:04 +0100

    Picture controller

---
+++ app/Http/Controllers/Api/PictureController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PictureRequest;
+use App\Http\Requests\PictureUpdateRequest;
+use App\Models\Picture;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class PictureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $picture = Picture::all();
+        return response()->json($picture);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), (new PictureRequest())->rules());
+        if ($validator->fails()) {
+            $errormsg = "";
+            foreach ($validator->errors()->all() as $error) {
+                $errormsg .= $error . " ";
+            }
+            $errormsg = trim($errormsg);
+            return response()->json($errormsg, 400);
+        }
+        $picture = new Picture();
+        $picture->fill($request->all());
+        $picture->fill([
+            'password' => Hash::make($request->input('password'))
+        ])->save();
+        return response()->json($picture, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $picture = Picture::find($id);
+
+        if (is_null($picture)) {
+            return response()->json(["message" => "A megadott azonosítóval nem található felhasználó."], 404);
+        }
+        return response()->json($picture);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PictureUpdateRequest $request, int $id)
+    {
+        if ($request->isMethod('PUT')) {
+            $validator = Validator::make($request->all(), (new PictureUpdateRequest())->rules());
+            if($validator->fails()) {
+                $errormsg = "";
+                foreach ($validator->errors()->all() as $error) {
+                    $errormsg .= $error . " ";
+                }
+                $errormsg = trim($errormsg);
+                return response()->json($errormsg, 400);
+            }
+        }
+
+        $picture = Picture::find($id);
+        if (is_null($picture)) {
+            return response()->json(["message" => "A megadott azonosítóval nem található felhasználó."], 404);
+        }
+
+        $picture->fill($request->all());
+        $picture->save();
+        return response()->json($picture, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $id)
+    {
+        $picture = Picture::find($id);
+        if (is_null($picture)) {
+            return response()->json(["message" => "A megadott azonosítóval nem található felhasználó."], 404);
+        }
+        Picture::destroy($id);
+        return response()->noContent();
+    }
+}
